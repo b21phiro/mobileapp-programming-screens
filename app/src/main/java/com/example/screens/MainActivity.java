c13109b68e42d6eb7eb5ee7c60a03e695ea48538
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-04-28 15:12:50 +0200

    Implemented submitButton and EditText functionality

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        EditText searchInput = (EditText) findViewById(R.id.searchInput);
+
+        submitButton.setOnClickListener(view -> {
+            String input = (String) searchInput.getText().toString();
+            submitSearchInput(input);
+        });
+
     }
 
-    public void submitSearchInput(View view, String input) {
+    public void submitSearchInput(String input) {
         Intent intent = new Intent(this, SearchActivity.class);
         intent.putExtra(EXTRA_MESSAGE, input);
         startActivity(intent);
