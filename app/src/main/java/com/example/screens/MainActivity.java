8ff3418b5866c4b57f8a8ff53f0e0621b50a9052
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-04-28 14:54:13 +0200

    Initiated intent activity for SearchActivity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "com.example.screens";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitSearchInput(View view, String input) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, input);
+        startActivity(intent);
+    }
+
 }
